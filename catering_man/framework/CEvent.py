d4e992c45fed5b56de59fd49358b15a28b86ffb5
unknown <joorhy@gmail.com>
2014-09-26 15:14:18 +0800

    modify framework package, for one model contains all class

--- catering_man/framework/CEvent.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*- 
-#!/usr/bin/env python
-import wx
-    
-class CEvent(wx.PyEvent):
-    def __init__(self, evt_id, evt_data):
-        wx.PyEvent.__init__(self)
-        self.SetEventType(evt_id)
-        self.data = evt_data
--- catering_man/framework/CSingleton.py
+++
@@ -1,9 +0,0 @@
-#_*_ encoding=utf-8 _*_
-
-class CSingleton(object):  
-    def __new__(cls, *args, **kw):  
-        if not hasattr(cls, '_instance'):  
-            orig = super(CSingleton, cls)  
-            cls._instance = orig.__new__(cls, *args, **kw)  
-            
-        return cls._instance  
